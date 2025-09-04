cff4e6db05a8cdbd35d23ab15c4878aace8a2900
W4lterRomero <rh23015@ues.edu.sv>
2025-09-04 12:09:46 -0600

    Creacion de objetos de array

---
+++ app/index.php
@@ -0,0 +1,24 @@
+<?php
+    $personajes = [
+       [ "nombre" =>"lord", "descripcion" => "el villain", "categoria" => "villano", "imagen" => "img/img1.jpg"],
+       [ "nombre" =>"el nena", "descripcion" => "cautiva con la voz", "categoria" => "amigo", "imagen" => "img/img2.jpg"],
+       [ "nombre" =>"shrek", "descripcion" => "el prota", "categoria" => "amigo", "imagen" => "img/img3.jpg"],
+       [ "nombre" =>"burro", "descripcion" => "el real prota", "categoria" => "amigo", "imagen" => "img/img4.jpg"],
+       [ "nombre" =>"gengibre", "descripcion" => "el real real prota", "categoria" => "amigo", "imagen" => "img/img5.jpg"],
+
+
+    ];
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+    ?>
+</body>
+</html>
