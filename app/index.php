aa649fad7af82091333edc7ae3030b7aea13340c
W4lterRomero <rh23015@ues.edu.sv>
2025-09-04 12:46:33 -0600

    Finalizaciòn del primer ejercicio y comienzo del tercer ejercicio

--- app/index.php
+++ app/index.php
@@ -8,17 +8,42 @@
 
 
     ];
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="">
 </head>
 <body>
-    
+    <form method="get">
+        <input type="text" name="categoria" placeholder="Ingrese categoría">
+        <input type="submit" value="Filtrar">
+    </form>
     <?php
-
+    $categoria = isset($_GET["categoria"]) ? trim($_GET["categoria"]) : '';
+    for ($i=0; $i < count($personajes); $i++) {
+        if ($categoria === '' || strtolower($personajes[$i]["categoria"]) === strtolower($categoria)) {
+            echo "
+        <div class='personajesFiltrados' style='display:flex; flex-direction: row; gap: 3rem; padding: 1rem;'>
+            <div class='PersonajesFiltrados_nombre'>
+                <p>{$personajes[$i]['nombre']} <br></p>
+            </div> <br>
+            <div class='personajesFiltrados_imagen'>
+                <img src='{$personajes[$i]['imagen']}' alt='{$personajes[$i]['nombre']}' style='width:100px;'>
+            </div>
+            <div class='personajesFiltrados_descripcion'>
+                <p>{$personajes[$i]['descripcion']} <br></p>
+            </div><br>
+            <div class='personajesFiltrados_categoria'>
+                <p>{$personajes[$i]['categoria']} <br></p>
+            </div><br>
+        </div>
+        ";
+        }
+    }
     ?>
 </body>
 </html>
